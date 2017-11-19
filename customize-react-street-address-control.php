8cf98677c6e240fb4e51fe10e9f0c100adac76fc
Weston Ruter <weston@xwp.co>
2017-11-18 20:23:48 -0800

    Add WP plugin file

---
+++ customize-react-street-address-control.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Customize React Street Address Control
+ * Description: Demonstration of a Customizer control that uses React for its interface.
+ * Version: 0.1.0
+ * Author: Weston Ruter, XWP
+ * Author URI: https://weston.ruter.net/
+ * License: GPLv2+
+ *
+ * @package Customize_React_Street_Address_Control
+ */
+
+namespace Customize_React_Street_Address_Control;
