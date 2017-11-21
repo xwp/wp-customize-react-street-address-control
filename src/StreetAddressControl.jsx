9495e8a9774a9a0891fe65512bcd52a3bc7065c2
Weston Ruter <weston@xwp.co>
2017-11-20 17:39:02 -0800

    Call ReactDOM.unmountComponentAtNode() when control is removed/destroyed
    h/t @gcorne

--- src/StreetAddressControl.jsx
+++ src/StreetAddressControl.jsx
@@ -21,11 +21,21 @@ const StreetAddressControl = wp.customize.Control.extend({
 	initialize: function( id, params ) {
 		const control = this;
 
-		// Bind for the sake of passing as prop to React component.
+		// Bind functions to this control context for passing as React props.
 		control.updateSetting = control.updateSetting.bind( control );
 		control.setNotificationContainer = control.setNotificationContainer.bind( control );
 
 		wp.customize.Control.prototype.initialize.call( control, id, params );
+
+		// The following should be eliminated with <https://core.trac.wordpress.org/ticket/31334>.
+		function onRemoved( removedControl ) {
+			if ( control === removedControl ) {
+				control.destroy();
+				control.container.remove();
+				wp.customize.control.unbind( 'removed', onRemoved );
+			}
+		}
+		wp.customize.control.bind( 'removed', onRemoved );
 	},
 
 	/**
@@ -59,6 +69,8 @@ const StreetAddressControl = wp.customize.Control.extend({
 	/**
 	 * Render the control into the DOM.
 	 *
+	 * This is called from the Control#embed() method in the parent class.
+	 *
 	 * @returns {void}
 	 */
 	renderContent: function renderContent() {
@@ -84,14 +96,34 @@ const StreetAddressControl = wp.customize.Control.extend({
 	 *
 	 * @returns {void}
 	 */
-	ready: function() {
+	ready: function ready() {
 		const control = this;
 
 		// Re-render control when setting changes.
 		control.setting.bind( () => {
 			control.renderContent();
 		} );
-	}
+	},
+
+	/**
+	 * Handle removal/de-registration of the control.
+	 *
+	 * This is essentially the inverse of the Control#embed() method.
+	 *
+	 * @link https://core.trac.wordpress.org/ticket/31334
+	 * @returns {void}
+	 */
+	destroy: function destroy() {
+		const control = this;
+
+		// Garbage collection: undo mounting that was done in the embed/renderContent method.
+		ReactDOM.unmountComponentAtNode( control.container[0] );
+
+		// Call destroy method in parent if it exists (as of #31334).
+		if ( wp.customize.Control.prototype.destroy ) {
+			wp.customize.Control.prototype.destroy.call( control );
+		}
+	},
 });
 
 export default StreetAddressControl;
